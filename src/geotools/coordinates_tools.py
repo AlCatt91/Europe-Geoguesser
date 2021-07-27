310277e0bff7227ed5431bb008e4156297d54664
Alberto <alberto.cattaneo91@gmail.com>
2021-07-27 16:37:21 +0200

    Added distance_check function

--- src/geotools/coordinates_tools.py
+++ src/geotools/coordinates_tools.py
@@ -23,44 +23,50 @@ class ImageTuple(fastuple):
         return show_image(torch.cat([t1,line,t2,line,t3], dim=2), ctx=ctx, **kwargs)
 
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
 def distance_check(learn: Learner, final_grid: pd.DataFrame, all : bool = True) -> None:
   """
-  Computes mean distance between the centroids of MultiPolygones in geometries and the predictions of learn, for elements in the validation dataloader.
+  Computes mean distance between the locations in the validation dataloader of learn and the predictions of the model.
   learn: model Learner
-  final_grid: DataFrame containing MultiPolygons in 'geometry' column and list of locations (as Point) in 'grid_points' column
-  all: if True, take as coordinates prediction the weighted average of all centroids; if False, consider only the centroid of the most likely MultiPolygon.
+  final_grid: DataFrame containing MultiPolygons in 'geometry' column and list of locations (as List[Point]) in 'grid_points' column
+  all: if True, take as coordinates prediction the weighted average of all MultiPolygon centroids; if False, consider only the centroid of the most likely MultiPolygon.
   """
   #Matrix of multipolygon centroids, indexed as the numerical labels in the Learner dataloaders
   centers = torch.tensor([[cell.centroid.x, cell.centroid.y] for cell in final_grid.loc[map(int, learn.dls.vocab), 'geometry']])
-  
-  squared_errors = []
 
-  for path in learn.dls.valid.items:
+  #Items of validation dataloader, divided into batches
+  batch_items = chunks(learn.dls.valid.items, learn.dls.valid.bs)
+
+  mean_batch_errors = []
+
+  for itms, (x, y) in zip(batch_items, learn.dls.valid):
     #Check whether the model takes as input a single image or an ImageTuple
-    if not isinstance(path, Path):
-      match = re.match(r".*_(\d+)_(\d+)_(\d+)$", path[0].stem).groups()
-      img = ImageTuple.create(path[:-1])
+    if isinstance(itms[0], Path):
+      #Extract infos on the location from path of item
+      match = [re.match(r".*_(\d+)_(\d+)_", s.stem).groups() for s in itms]
     else:
-      match = re.match(r".*_(\d+)_(\d+)_(\d+)$", path.stem).groups()
-      img = PILImage.create(path)
+      match = [re.match(r".*_(\d+)_(\d+)_", s[0].stem).groups() for s in itms]
+    #Recover Points of locations from final_grid['grid_points']
+    pts = [final_grid.loc[int(a[0]), 'grid_points'][int(a[1])] for a in match]
+    #True coordinates of locations for images in the batch
+    targ_coords = torch.tensor([(pt.x, pt.y) for pt in pts]).reshape(-1, 2)
 
-    cell, img_id = match[0], int(match[1])
-    pt = final_grid.loc[int(cell), 'grid_points'][img_id]
-    #True coordinates of the location
-    targ_coords = torch.tensor([pt.x, pt.y]) 
-
-    _, _, probs = learn.predict(img)
+    #Probabilities of cells
+    probs, _ = learn.get_preds(dl=[(x,y)])
     if all:
       #Prediction is weighted average of centroids
       pred_coords = probs @ centers
     else:
       #Prediction is the centroid of the most likely cell
-      pred_coords = centers[int(torch.argmax(probs))]
-    #Append to squared_errors the squared Euclidean distance between the true location and the predicted one 
-    squared_errors.append(((targ_coords - pred_coords) ** 2).sum())
+      pred_coords = centers[probs.argmax(-1)]
+    #Append to mean_batch_errors the mean squared Euclidean distance between the true location and the predicted one 
+    mean_batch_errors.append(((targ_coords - pred_coords) ** 2).sum(1).mean())
 
-  print(f"Validation RMSE: {np.mean(np.sqrt(squared_errors)):.4f}")
-  
+  print(f"Validation RMSE: {np.sqrt(np.mean(mean_batch_errors)):.4f}")
   
 
 def predict_coords(probs: torch.tensor, final_grid: pd.DataFrame, vocab: list) -> torch.tensor:
